06bbbed67c428ccef8a1954eac8a603a5456c9c2
Mohit Hooda <mhooda87@gmail.com>
2019-01-21 16:03:06 +0530

    Update BostonHousing.R

--- BostonHousing.R
+++ BostonHousing.R
@@ -63,3 +63,51 @@ median(boston.df$CRIM)
 is.na(boston.df$CRIM)  #To find missing values in CRIM
 sum(is.na(boston.df$CRIM)) #To count the missing value
 sapply(boston.df,mean) #Mean of all rows
+round(cor(boston.df),2) #To find corelation ;QUOTE: Co-relation is not Cause-ation
+#Range of Corelation [-1,1] (Universal)
+
+#-------------------------------------------------------------------------------
+#New Exercises
+  
+  
+boston.df2 <- boston.df  #Making a new Data frame which takes previous data frame
+rows.to.missing<-sample(row.names(boston.df2),10)#Pick 10 random rows 
+boston.df2[rows.to.missing,]$CRIM<-NA#Assign them NA
+summary(boston.df2$CRIM) #Checking for NA
+
+
+is.na(boston.df2$CRIM)
+sum(is.na(boston.df2$CRIM))
+
+
+boston.df2[rows.to.missing,]$CRIM <-median(boston.df2$CRIM,na.rm=TRUE)
+#^Allocation the median of non-na to na fields;
+#^Allocation of median/mean/... to na's is known as IMPUTATION(universal)
+
+#Q: Advantage of picking median over mean?
+#Ans: Outliers deviate mean largely but does effect median.
+
+#----------------------------------------------------------------------------
+#VISUALISATION OF DATA
+#PLOTS
+#1.Scatter Plot(Numerical Values)
+plot(boston.df$MEDV~boston.df$LSTAT,xlab="MEDV",ylab="LSTAT")
+
+#2.GG (grammar for graphics) Plot
+library(ggplot2)
+ggplot(boston.df)+geom_point(aes(x=LSTAT,y=MEDV),colour="navy",alpha=0.7)
+
+#3.Adding categorical values by colour-coding
+
+plot(boston.df$NOX~boston.df$LSTAT,ylab="NOX",xlab="LSTAT",col=ifelse(boston.df$CHAS==1,"black","gray"))
+
+#Barchart
+#for CHAS vs mean MEDV
+#compute mean MEDV per CHAS=(0,1)
+aggregate(boston.df$MEDV,by=list(boston.df$CHAS),FUN=mean)
+#^Mean of median values of close to charles river and away from charles river
+list(boston.df$CHAS)
+
+data.for.plot<-aggregate(boston.df$MEDV,by=list(boston.df$CHAS),FUN=mean)
+names(data.for.plot)<-c("CHAS","MeanMEDV")
+barplot(data.for.plot$MeanMEDV,names.arg=data.for.plot$CHAS,xlab = "CHAS",ylab="Avg. MEDV")
