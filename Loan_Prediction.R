ece56f1f678840411f846511f88a96bac796cb27
Mohit Hooda <mhooda87@gmail.com>
2019-01-28 17:32:44 +0530

    Create Loan_Prediction.R

---
+++ Loan_Prediction.R
@@ -0,0 +1,11 @@
+
+#Data Partitioning into Train & Validation 
+train<- read.csv('D:\\Mohit\\R\\Loan_PredictionTrain.csv',header = TRUE)
+summary(train)
+train
+test <-  read.csv('D:\\Mohit\\R\\Loan_PredictionTest.csv',header = TRUE)
+summary(test)
+test
+
+#Logistics Regressing Model
+
