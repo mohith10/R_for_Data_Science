465b86dd5bc77bb0dfb8d71c5f934fe633c5af33
Mohit Hooda <mhooda87@gmail.com>
2019-04-08 15:53:03 +0530

    Create Post_Mid_term.R

---
+++ Post_Mid_term.R
@@ -0,0 +1,16 @@
+#Ensemble Model - Powerful way of participating in challenges and contests
+#methods-Bagging and Boosting
+#sTEPS- gET-> cLEAN DATA ->tRAIN mODEL -> Test Data -> Improve
+#We partition the data to avoid overfitting - Basic Question
+#In unsupervised we dont have any target variable.
+
+#Bagging - Bootstrap Aggregating
+
+
+
+
+
+
+
+
+
